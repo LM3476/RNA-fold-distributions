b432556c130c5405d7f49ba25f0e341008ee17fb
LM3476 <lachjkmcd01@gmail.com>
2022-12-08 20:34:28 +0800

    Update distribution-grapher.py

--- distribution-grapher.py
+++ distribution-grapher.py
@@ -1,20 +1,22 @@
-import math
 import RNA
+import math
 import random
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
-#from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit
 from os.path import exists
 from scipy.integrate import quad
 from scipy.stats import ks_2samp
 
+# CHANGE THESE GLOBALS
+# Can specify a seed to generate pseudorandom sequences
 # SEED = 827865
 SEED = None
-NUMBER_OF_SEQUENCES = 10
-LENGTH = 100
+NUMBER_OF_SEQUENCES = 20
+LENGTH = 20
 SAMPLE_SIZE = 100000
 bases =["A","G","C","U"]
 
@@ -30,7 +32,6 @@ def generate_output(sequence, sample_size):
 def create_random_sequence(seed, length):
     if seed != None:
         random.seed(seed)
-    # print(''.join(random.choices(bases, k=length)))
     return ''.join(random.choices(bases, k=length))
 
 #CODE FOR PLOTTING DISTRIBUTIONS MOVED TO THIS FILE
@@ -51,20 +52,19 @@ def version_2(df):
         freq_df = row.value_counts().rename_axis('unique_values').reset_index(name='counts')
         freq_df['rank'] = np.arange(1,freq_df.shape[0]+1)
         freq_df['frequency'] = freq_df['counts']/SAMPLE_SIZE
-        # print(freq_df)
+
         x_data = freq_df[['rank']].to_numpy()
         y_data = freq_df[['frequency']].to_numpy()
-        # print(x_data,y_data)
-        # freq_df.plot.bar()
+
         # plt.loglog(x_data,y_data)
         plt.yscale("log")
         plt.plot(x_data,y_data,'grey')
-        # print(kl_divergence(y_data,y))
         avg_df[i] = freq_df['frequency']
+
     avg_df['mean'] = avg_df.mean(axis=1)
     avg_df['rank'] = np.arange(1,avg_df.shape[0]+1)
-    x_avg = avg_df[['rank']].to_numpy()
-    y_avg = avg_df[['mean']].to_numpy()
+    x_avg = avg_df[['rank']].to_numpy().flatten()
+    y_avg = avg_df[['mean']].to_numpy().flatten()
     plt.plot(x_avg, y_avg,'r')
 
 def record_divergences(df):
@@ -106,8 +106,8 @@ def arrange_data(df_row):
     y_data = freq_df[['counts']].to_numpy()
     return x_data, y_data
 
-
-
+### Saves plot as a png image
+### Naming follows convention: "[number of sequences]-[length of sequences]-[number of sample structures per sequence]-[number].png"
 def write_plot_to_file():
     file_suffix = 1
     file_exists = True
@@ -118,6 +118,7 @@ def write_plot_to_file():
     print(filename)
     plt.savefig(filename)
 
+### Plots inverse distribution with numerator a and exponent b
 def plot_inverse_distribution(a,b):
     f = lambda x:a/(x**b)
     k, err = quad(f,1,LENGTH*10)
@@ -130,16 +131,24 @@ def plot_inverse_distribution(a,b):
     plt.yscale("log")
     plt.plot(x_graph, y_graph, 'b')
     # plt.plot(x,y)
-    
+
+### HELPER FUNCTIONS ### 
+
+### Gets kl_divergence
 def kl_divergence(p, q):
 	return sum(p[i] * np.log2(p[i]/q[i]) for i in range(min(len(p),len(q))))
 
+### Helper functions for curve fitting
 def test_func(x, a, b):
     return a/(x**b)
 
+def test_func_1var(x, c):
+    return 1/(x**c)
 
-
-def main():
+### MAIN FUNCTIONS ###
+### Run this if you only want to generate output for one sequence
+### Uncomment print statements to see KL divergences
+def get_curve_fit():
     open("csvoutput.csv","w").close() #initialises output file
     fw = open("csvoutput.csv","w")
     for _ in range(NUMBER_OF_SEQUENCES):
@@ -151,16 +160,34 @@ def main():
     fw.close()
 
     df = pd.read_csv("csvoutput.csv", header=None)
-    plot_inverse_distribution(1,4)
+    # plot_inverse_distribution(0.045,0.777)
     version_2(df)
     # print("AVERAGE KL DIVERGENCE BETWEEN ALL PAIRS: ",record_divergences(df)[0])
-    print("AVERAGE KL DIVERGENCE WITH AVERAGE GRAPH: ",record_divergences_against_average(df)[0])
-    print("AVERAGE KL DIVERGENCE WITH FUNCTION: ",record_divergences_against_function(df))
+    # print("AVERAGE KL DIVERGENCE WITH AVERAGE GRAPH: ",record_divergences_against_average(df)[0])
+    # print("AVERAGE KL DIVERGENCE WITH FUNCTION: ",record_divergences_against_function(df))
 
-    # print("AVERAGE KS DIVERGENCE BETWEEN ALL PAIRS: ",record_divergences(df)[1])
-    # print("AVERAGE KS DIVERGENCE WITH AVERAGE GRAPH: ",record_divergences_against_average(df)[1])
+    # write_plot_to_file()
+    # plt.show()
 
-    write_plot_to_file()
-    plt.show()
+    best_fit_params = curve_fit(test_func, x_avg, y_avg)
+    # best_fit_params = curve_fit(test_func_1var, x_avg, y_avg) [USE IF CURVEFITTING WITH test_func_1var()]
+    print(best_fit_params)
+    return best_fit_params
+
+### Cycles through various lengths (rewrites the 'LENGTH' global variable) to curve fit the average distribution for each
+def main():
+    global LENGTH
+    open("curvefits_2.csv","w").close() #initialises output file
+    fw = open("curvefits_2.csv","w")
+    fw.writelines("LENGTH,a,b,error,\n")
+    for seq_len in range(10,110,10):
+        LENGTH = seq_len
+        print("LENGTH: "+str(LENGTH))
+        best_fit_params = get_curve_fit()
+        fw.writelines(str(LENGTH)+",")
+        fw.writelines(str(best_fit_params[0][0])+","+str(best_fit_params[0][1])+","+str(best_fit_params[1][0])+","+str(best_fit_params[1][1]))
+        # fw.writelines(str(best_fit_params[0])+","+str(best_fit_params[1])) [USE IF CURVEFITTING WITH test_func_1var()]
+        fw.writelines('\n')
+    fw.close()
 
 main()
